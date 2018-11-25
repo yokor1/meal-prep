770e7229b8f8f4d76f5067647a6c93fbb37e8e66
youcef <youcef.korichi.1@ulaval.ca>
2018-11-25 18:40:24 -0500

    added layout

---
+++ src/components/AppDrawer.vue
@@ -0,0 +1,38 @@
+<template>
+    <v-navigation-drawer app fixed :value="drawer" stateless>
+        <v-list dense>
+            <v-list-tile>
+                <v-list-tile-action>
+                    <v-icon>home</v-icon>
+                </v-list-tile-action>
+                <v-list-tile-content>
+                    <v-list-tile-title>Home</v-list-tile-title>
+                </v-list-tile-content>
+            </v-list-tile>
+            <v-list-tile>
+                <v-list-tile-action>
+                    <v-icon>contact_mail</v-icon>
+                </v-list-tile-action>
+                <v-list-tile-content>
+                    <v-list-tile-title>Contact</v-list-tile-title>
+                </v-list-tile-content>
+            </v-list-tile>
+        </v-list>
+    </v-navigation-drawer>
+
+</template>
+
+<script>
+export default {
+    name: 'AppDrawer',
+    props: ['drawer'],
+    data() {
+        return {
+            dr: this.drawer
+        };
+    }
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/AppNavigation.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-toolbar app color="brown darken-4" dark fixed>
+        <v-toolbar-side-icon @click.stop="$emit('toggleDrawerEvent')"></v-toolbar-side-icon>
+        <v-toolbar-title>Application</v-toolbar-title>
+        <v-btn flat>Menu</v-btn>
+        <v-spacer></v-spacer>
+        <v-btn flat>SIGN IN</v-btn>
+        <v-btn color="brown lighten-3">JOIN</v-btn>
+    </v-toolbar>
+</template>
+<script>
+export default {
+    name: 'AppNavigation'
+};
+</script>
